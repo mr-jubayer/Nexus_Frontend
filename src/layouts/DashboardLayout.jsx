9323676c6057981a72c756c44e2b3a6d3bed93a3
jubayer <jubayerahmed46@gmail.com>
2025-01-17 19:14:43 +0600

    feautes updated

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,50 +1,28 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import ReapopToaster from "../components/reapopToaster";
 import PageLoader from "../components/spinners/PageLoader";
 import useAuth from "../hooks/useAuth";
 import SideBar from "../pages/dashboard/sidebar/SideBar";
-import { useState } from "react";
-import { FaBookReader, FaUser } from "react-icons/fa";
-import { TbBrandSoundcloud } from "react-icons/tb";
-
-const adminRoutes = [
-  {
-    path: "users",
-    value: "Users",
-    icon: <FaUser />,
-  },
-  {
-    path: "articles",
-    value: "articles",
-    icon: <FaBookReader />,
-  },
-  {
-    path: "add-publisher",
-    value: "Add Publisher",
-    icon: <TbBrandSoundcloud />,
-  },
-];
 
 function DashboardLayout() {
-  const [curentRoute, setCurrentRoute] = useState();
   const { loading } = useAuth();
+  const location = useLocation();
+  const routeName = location.pathname.split("/")[2];
+  const modifiedRoute = routeName[0].toUpperCase().concat(routeName.slice(1));
 
-  const settingRoute = (route) => {
-    setCurrentRoute(route);
-  };
   if (loading) return <PageLoader />;
   return (
     <div>
       <div className=" bg-gray-100 flex">
         <div>
-          <SideBar adminRoutes={adminRoutes} settingRoute={settingRoute} />
+          <SideBar />
         </div>
 
         {/* dynamic page */}
-        <div className="max-w-7xl w-full  p-6 mx-auto">
-          <div className="text-sm mb-6">
+        <div className="max-w-7xl w-full  md:p-6 p-3  mx-auto">
+          <div className="text-sm mb-6 md:pl-0 pl-8">
             <span className="text-gray-500">Dashboard /</span>{" "}
-            <span className="font-semibold">Home</span>
+            <span className="font-semibold">{modifiedRoute}</span>
           </div>
 
           <Outlet />
--- src/pages/dashboard/sidebar/SideBar.jsx
+++ src/pages/dashboard/sidebar/SideBar.jsx
@@ -1,28 +1,79 @@
 import { NavLink } from "react-router";
 import logo from "../../../assets/logo2.png";
+import { MdOutlineMenu } from "react-icons/md";
+import { FaBookReader, FaUser } from "react-icons/fa";
+import { TbBrandSoundcloud } from "react-icons/tb";
+import { useRef } from "react";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
 
-export default function SideBar({ adminRoutes }) {
+export default function SideBar() {
+  const asideRef = useRef();
+  const adminRoutes = [
+    {
+      path: "users",
+      value: "Users",
+      icon: <FaUser />,
+    },
+    {
+      path: "articles",
+      value: "articles",
+      icon: <FaBookReader />,
+    },
+    {
+      path: "add-publisher",
+      value: "Add Publisher",
+      icon: <TbBrandSoundcloud />,
+    },
+  ];
+
+  const handleSidebar = () => {
+    // show sidebar
+    asideRef.current.classList.remove("-translate-x-72");
+  };
+  const closeSlideBar = () => {
+    // show sidebar
+    asideRef.current.classList.add("-translate-x-72");
+  };
+
   return (
-    <aside className="w-64 bg-white shadow-md h-screen">
-      <div className="p-4 border-b border-gray-200">
-        <img src={logo} alt="brand name" className="md:h-9 h-[24px]" />
-      </div>
-      <nav className="mt-4 space-y-2">
-        {adminRoutes.map((route) => (
-          <NavLink
-            key={route.value}
-            to={route.path}
-            className={({ isActive }) =>
-              `${isActive ? "border-l-4 bg-green-100" : ""} flex items-center px-4 py-2 text-gray-700 hover:bg-green-100 transition-all duration-200 border-l-green-600  gap-2 `
-            }
-          >
-            <span>{route.icon}</span>
-            {route.value}
-          </NavLink>
-        ))}
+    <div className="relative">
+      <button
+        className="text-xl cursor-pointer md:hidden top-2 left-2 rounded-md absolute hover:bg-slate-100 focus:bg-slate-200 transition-all duration-200 p-1"
+        onClick={handleSidebar}
+      >
+        <MdOutlineMenu />
+      </button>
+      <aside
+        ref={asideRef}
+        className="transition-all duration-300 w-64 bg-white md:shadow-md shadow-2xl h-screen md:relative fixed top-0 left-0  md:-translate-x-0 -translate-x-72"
+      >
+        <div className="p-4 border-b border-gray-200 flex justify-between">
+          <img
+            src={logo}
+            alt="brand name"
+            className="md:h-9 md:block  h-[20px]"
+          />
+          <button onClick={closeSlideBar} className="md:hidden">
+            <MdOutlineArrowBackIosNew className="text-xl" />
+          </button>
+        </div>
+        <nav className="mt-4 space-y-2">
+          {adminRoutes.map((route) => (
+            <NavLink
+              key={route.value}
+              to={route.path}
+              className={({ isActive }) =>
+                `${isActive ? "border-l-4 bg-green-100" : ""} flex items-center px-4 py-2 text-gray-700 hover:bg-green-100 transition-all duration-200 border-l-green-600  gap-2 `
+              }
+            >
+              <span>{route.icon}</span>
+              {route.value}
+            </NavLink>
+          ))}
 
-        <div className="mt-4 border-t pt-4 border-gray-200"></div>
-      </nav>
-    </aside>
+          <div className="mt-4 border-t pt-4 border-gray-200"></div>
+        </nav>
+      </aside>
+    </div>
   );
 }
